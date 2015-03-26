2ab53496345f6cd83a43e3c7457815b65904d649
gfportfolio <gbfarns16@gmail.com>
2015-03-26 09:44:16 -0600

    Added container object

---
+++ StuffTracker/app/src/main/java/benandfriends/stufftracker/Container.java
@@ -0,0 +1,41 @@
+package benandfriends.stufftracker;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+/**
+ * Created by gavinfarnsworth on 3/26/15.
+ */
+public class Container {
+    private String name;
+    private Bitmap image;
+    private Location location;
+
+    public Container(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
