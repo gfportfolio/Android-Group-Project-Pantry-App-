eff838f328a1957b8e07a30f02ed1959135834fd
nvalletta <nora.valletta@gmail.com>
2015-04-09 08:19:08 -0600

    Add Location Activity
    Adds view stuff for adding a location.

--- StuffTracker/app/src/main/java/benandfriends/stufftracker/activities/MainActivity.java
+++ StuffTracker/app/src/main/java/benandfriends/stufftracker/activities/MainActivity.java
@@ -1,11 +1,13 @@
 package benandfriends.stufftracker.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.melnykov.fab.FloatingActionButton;
 
@@ -18,7 +20,6 @@ import benandfriends.stufftracker.utilities.LocationsAdapter;
 
 public class MainActivity extends Activity {
 
-    private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
 
@@ -27,16 +28,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.recyclerView = (RecyclerView) findViewById(R.id.locations_recycler_view);
-        this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        this.recyclerView.setHasFixedSize(true);
-
         this.adapter = new LocationsAdapter(this.getMockDataSet());
-        this.recyclerView.setAdapter(this.adapter);
+        this.setUpViews();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.attachToRecyclerView(this.recyclerView);
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.adapter.notifyDataSetChanged();
     }
 
 
@@ -46,6 +46,29 @@ public class MainActivity extends Activity {
             dataSet.add("Test " + i);
         }
         return dataSet;
+    }
+
+
+    private void setUpViews() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.locations_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(this.adapter);
+
+        this.instantiateFab(recyclerView);
+    }
+
+
+    private void instantiateFab(RecyclerView recyclerView) {
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.attachToRecyclerView(recyclerView);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateContainerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
