70ffeef37183022e13c0474dd7e064bd40ae299e
gfportfolio <gbfarns16@gmail.com>
2015-04-14 09:33:12 -0600

    added onclick to cards

--- StuffTracker/app/src/main/java/benandfriends/stufftracker/MyAdapter.java
+++ StuffTracker/app/src/main/java/benandfriends/stufftracker/MyAdapter.java
@@ -38,6 +38,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ItemViewHolder> im
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.card_view, parent, false);
+
+
+        v.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.i("Click",v.getId()+"");
+
+            }
+        });
         ItemViewHolder vh = new ItemViewHolder( v);
 
         return vh;
@@ -49,7 +59,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ItemViewHolder> im
         holder.vName.setText(i.getTitle());
         holder.vLocation.setText(i.getCount()+"");
         holder.vImg.setImageResource(i.getImage());
-
+        holder.itemView.setId(position);
     }
 
 
